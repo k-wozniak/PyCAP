a8bcd1a5e68f31de1c7c842b948e4bfde9291dbc
k-wozniak <kamil.krzysztof.wozniak@outlook.com>
2021-06-16 00:17:50 +0100

    Add scritps to compare different methods on the vegus dataset

--- examples/ex_vegus_nerve_distribiution.py
+++ examples/ex_vegus_nerve_distribiution.py
@@ -6,16 +6,40 @@ import numpy as np
 from scipy.io import loadmat, savemat
 import matplotlib.pyplot as plt
 
+def process_distributions(diss, resolution, target_resolution, min_cv, max_cv):
+    # resolution = 0.1
+    # target_resolution = 1
+    padding = round(target_resolution/(2*resolution))
+
+    # perform resolution scaling
+    diss = np.sum(diss, 0)
+    diss = np.pad(diss, (padding, padding), 'constant')
+    
+    mask = np.ones(padding*2)
+    mask[0] = 0.5
+    mask[-1] = 0.5
+
+    diss = np.convolve(mask, diss, 'valid')
+    diss = diss[::(padding*2)] # start:stop:step
+
+    # perform velocity scaling (Should be replaced my matrix multiplication)
+    new_cv_range = np.arange(min_cv, max_cv + (target_resolution/2), target_resolution)
+    diss = diss / (new_cv_range**2)
+
+    return diss
+
 #caps = loadmat("meanCAP2.mat")['d']
-caps = loadmat("meanCAP2scaled.mat")['out']
+#caps = loadmat("meanCAP2scaled.mat")['out']
 #caps = loadmat("CAP.mat")['d']
 
-samples = 1
-signal_length, num_electrodes = caps.shape
-#signal_length, num_electrodes, samples = caps.shape
+data = loadmat("eCAPs_with_classes")
 
-caps = np.array(caps).T
-caps = np.flip(caps, 0)
+keys = np.array(data['keys'][0])
+caps = np.array(data['samples'])
+
+stimulation_levels = np.sort(np.unique(keys))[-5:]
+
+num_electrodes, signal_length, _ = caps.shape
 
 fs = 100e3 # Hz
 du = 3.5e-3
@@ -24,61 +48,62 @@ distance_first_electrode = 80e-3
 #caps = np.delete(caps, (4), axis=0)
 electrode_positions = (np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) * du) + distance_first_electrode
 
-for resolution in [0.1]:
-    search_range = np.arange(10, 120, resolution)
+# Search definitions
+resolution = 0.1
+target_resolution = 1
+min_cv = 10
+max_cv = 120
+search_range = np.arange(min_cv, max_cv, resolution)
 
-    qs = []
-    for pos in electrode_positions:
-        q = generate_q(signal_length, pos, search_range, fs)
-        qs.append(q)
+# Pre-calculate qs as they do not change between runs
+qs = []
+for pos in electrode_positions:
+    q = generate_q(signal_length, pos, search_range, fs)
+    qs.append(q)
+
+results = {}
+for stimulation_level in stimulation_levels:
+    print("Stimulation Level: " + str(stimulation_level))
     
-    ws = []
-    ws_lin = []
-    ws_quad = []
+    positions = [i for i, x in enumerate(keys) if x == stimulation_level]
 
-    for s in range(samples): # samples):
-        #signals = (caps[s, :, :])
-        signals = caps
-        w = be.NCap(signals, qs)
+    # Methods used to determine distributions
+    diss_mean_two_CAP = []
+    diss_NCAP = []
+    diff_NCAP_Pairs = []
+    diss_VSR = []
 
-        w_lin = w.copy()
-        w_quad = w.copy()
 
-        for i in range(len(w)):
-            v = search_range[i]
+    for s in positions:
+        signals = (caps[:, :, s])
+        
+        diss_mean_two_CAP.append(be.mean_two_cap(signals, qs))
+        diss_NCAP.append(be.NCap(signals, qs))
+        diff_NCAP_Pairs.append(be.NCapPairs(signals, qs))
+        diss_VSR.append(be.VSR(signals, fs, du, min_cv, target_resolution, max_cv+1))
+    
 
-            w_lin[i] = w[i] / v
-            w_quad[i] = w[i] / (v**2)
+    # Process multiple distributions into one
+    diss_mean_two_CAP = process_distributions(diss_mean_two_CAP, resolution, target_resolution, min_cv, max_cv)
+    diss_NCAP = process_distributions(diss_NCAP, resolution, target_resolution, min_cv, max_cv)
+    diff_NCAP_Pairs = process_distributions(diff_NCAP_Pairs, resolution, target_resolution, min_cv, max_cv)
+    diss_VSR = np.mean(diss_VSR, axis=0)
 
-        ws.append(w)
-        ws_lin.append(w_lin)
-        ws_quad.append(w_quad)
+    # Save results
+    results[stimulation_level] = {
+        "mean_two_cap": diss_mean_two_CAP,
+        "ncap": diss_NCAP,
+        "ncap_pairs": diff_NCAP_Pairs,
+        "VSR": diss_VSR,
+    }
 
-    # upscale the distributions
-    #ws1 = np.pad(ws_quad[0], (5, 5), 'constant')
-    #mask = [0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5]
-    #ws1 = np.convolve(mask, ws1, 'valid')
-    #ws1 = ws1[::10] # start:stop:step
-    #ws1 = ws1 / np.sum(ws1)
+items = results.items()
+to_save = {
+    "mean_two_cap": [value["mean_two_cap"] for _, value in items],
+    "ncap": [value["ncap"] for _, value in items],
+    "ncap_pairs": [value["ncap_pairs"] for _, value in items],
+    "VSR": [value["VSR"] for _, value in items],
+    "stimulations": [key for key, _ in items],
+}
 
-    #plt.plot(np.mean(ws, axis=0))
-    #plt.plot(np.mean(ws_lin, axis=0))
-    #plt.plot(search_range, ws_quad)
-    #search_range = np.arange(10, 80, 0.25)
-    #search_range = search_range[1:-1]
-    #plt.plot(ws1)
-    #plt.show()
-    ws_quad[0, -20:] = 0
-    ws_quad[0] = ws_quad[0] / sum(ws_quad[0])
-
-    plt.bar(search_range, ws_quad[0])
-    plt.bar(search_range, moving_average(ws_quad[0], 3))
-    plt.show()
-
-    plt.plot(search_range, ws[0])
-    plt.bar(search_range, moving_average(ws[0], 3))
-    plt.show()
-
-    file_name = "w_distribution" + str(resolution) + ".mat"
-    savemat(file_name, {"ws": ws, "ws_lin": ws_lin, "w_quad": ws_quad})
-
+savemat("loads_of_data_to_sort.mat", to_save)
